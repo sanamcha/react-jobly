07be4d4a5b6631bd71b12c4599875b6f4bc45e59
sanamcha <mhr.sanam@gmail.com>
2022-03-26 14:45:22 -0700

    jobly app for frontend

---
+++ frontend/react-jobly/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import UserContext from "./form/userContext";
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Jobly React App</h1>
+    </div>
+  );
+}
+
+export default App;
