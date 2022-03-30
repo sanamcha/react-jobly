b16aee6e549b1e27e6b0dab700be66d5c2c94295
sanamcha <mhr.sanam@gmail.com>
2022-03-29 22:12:28 -0700

    update homepage

---
+++ frontend/react-jobly/src/homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import  { useContext } from react;
+// import { Link } from "react-router-dom";
+// import UserContext from "../auth/UserContext";
+
+
+function Homepage(){
+    
+    return (
+        <div className="Home">
+            <h1>Just Home page...</h1>
+        </div>
+    )
+}
+
+export default Homepage;
