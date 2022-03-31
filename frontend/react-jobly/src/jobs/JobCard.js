82fc93edad75697664974e9a57dd9a3e1f23d56e
sanamcha <mhr.sanam@gmail.com>
2022-03-31 15:33:36 -0700

    modified job cards

--- frontend/react-jobly/src/jobs/JobCard.js
+++ frontend/react-jobly/src/jobs/JobCard.js
@@ -1,15 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState , useEffect } from "react";
+import UserContext from "../users/UserContext";
 
 
 //JobCardList -> JobCard
 
-function JobCard ({ id, title, companyName, Salary, equity }) {
-    console.log("JobCard");
+function JobCard ({ id, title, companyName, salary, equity }) {
+   const { hasApplied, apply } = useContext(UserContext);
+   const [applied, setApplied] =useState();
+
+   useEffect( function updateApplied(){
+       setApplied(hasApplied(id));
+   }, [id, hasApplied]);
+
+   //apply to job
+   async function handleApply(e){
+       if(hasApplied(id)) return;
+       apply(id);
+       setApplied(true);
+   }
+
 
     return (
-        <div>
-            <h1>This is Jobcard page....</h1>
+        <div className="JobCard card">{applied}
+          <div className="card-body">
+            <h6 className="card-title">{title}</h6>
+            <p>{companyName}</p>
+            {salary && <div><small>Salary: {salary}</small></div>}
+          {equity !== undefined && <div><small>Equity: {equity}</small></div>}
+          <button
+              className="btn btn-danger font-weight-bold text-uppercase float-right"
+              onClick={handleApply}
+              disabled={applied}
+          >
+            {applied ? "Applied" : "Apply"}
+          </button>
+          </div>  
         </div>
     )
 
