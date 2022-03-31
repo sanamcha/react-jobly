cf76b103582ddc30d49dfe92c952eb694acf8986
sanamcha <mhr.sanam@gmail.com>
2022-03-30 21:47:37 -0700

    added routes

--- frontend/react-jobly/src/App.js
+++ frontend/react-jobly/src/App.js
@@ -1,13 +1,49 @@
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { NavLink, Switch, Route, Redirect } from "react-router-dom";
+import JoblyApi from "./api";
 import './App.css';
-import UserContext from "./form/userContext";
+// import UserContext from "./form/userContext";
+import CompanyList from "./companies/CompanyList";
+import JobList from "./jobs/JobList";
+import CompanyDetail from "./companies/CompanyDetail";
 
+//storing token in local-storage
+export const TOKEN_ID = "jobly-token";
 
 function App() {
+  // const [currentUser, setCurrentUser] = useState([]);
+
+
+
+
   return (
-    <div className="App">
-     <h1>Jobly React App</h1>
-    </div>
+   <BrowserRouter>
+   <Switch >
+    {/* <UserContext.Provider */}
+      {/*  value={{ currentUser}} */}
+    {/*  > */}
+      <div className="App">
+        {/* <Navigation />
+        <Routes /> */}
+      <NavLink to="/companies">Companies</NavLink>
+      <NavLink to="/jobs">Jobs</NavLink>
+      <Route exact path="/companies">
+        <CompanyList />
+      </Route>
+      <Route exact path="/jobs">
+        <JobList />
+      </Route>
+      <Route exact path="/companies/:handle">
+        <CompanyDetail />
+      </Route>
+      <Redirect to="/" />
+        <h1>HOMEPAGE.......</h1>
+      </div>
+    {/* </UserContext.Provider> */}
+    </Switch>
+   </BrowserRouter>
   );
 }
 
