829b7dfeabccc3fb99e35ebf04e688c7bdc6cda1
sanamcha <mhr.sanam@gmail.com>
2022-03-30 21:49:37 -0700

    added company list

--- frontend/react-jobly/src/companies/CompanyList.js
+++ frontend/react-jobly/src/companies/CompanyList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import JoblyApi from "../api";
-
+import CompanyCard from "./CompanyCard";
 
 function CompanyList(){
     const [companies, setCompanies] = useState([]);
@@ -8,12 +8,18 @@ function CompanyList(){
     return (
         <div className="CompanyList">
             <h1>This is company page...</h1>
-            <CompanyCard 
-            key={c.handle}
-            handle={c.handle}
-            name={c.name}
-            description={c.description}
-            logoUrl={c.logoUrl} />
+            <div>
+                {companies.map(c => (
+                    <CompanyCard 
+                    key={c.handle}
+                    handle={c.handle}
+                    name={c.name}
+                    description={c.description}
+                    logoUrl={c.logoUrl} />
+                ))}
+
+            </div>
+            
         </div>
     )
 }
