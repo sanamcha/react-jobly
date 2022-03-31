0f010b8b99d4adc614a344d29fdeecd0c86b1091
sanamcha <mhr.sanam@gmail.com>
2022-03-31 07:49:25 -0700

    added company list form

--- frontend/react-jobly/src/companies/CompanyList.js
+++ frontend/react-jobly/src/companies/CompanyList.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from "react";
 import JoblyApi from "../api";
 import CompanyCard from "./CompanyCard";
+import SearchForm from "../SearchForm";
 
 function CompanyList(){
     const [companies, setCompanies] = useState([]);
 
+    useEffect(function getCompaniesOnLoad() {
+        search();
+    }, []);
+
+    //for search 
+    async function search(name) {
+        let companies = await JoblyApi.getCompanies(name);
+        setCompanies(companies);
+    }
+
     return (
         <div className="CompanyList">
-            <h1>This is company page...</h1>
-            <div>
-                {companies.map(c => (
-                    <CompanyCard 
-                    key={c.handle}
-                    handle={c.handle}
-                    name={c.name}
-                    description={c.description}
-                    logoUrl={c.logoUrl} />
-                ))}
-
-            </div>
-            
+            <SearchForm searchFor={search} />
+          {companies.length ? (
+              <div className="CompaniesList">
+              {companies.map(c => (
+                  <CompanyCard 
+                  key={c.handle}
+                  handle={c.handle}
+                  name={c.name}
+                  description={c.description}
+                  logoUrl={c.logoUrl} />
+              ))}
+               </div>
+          ) : (
+          <p>No, resuts found..</p>
+          )}
+    
         </div>
-    )
+    );
 }
 
 export default CompanyList;
