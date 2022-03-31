ab4dfe36b981b082847b7357735f3e0702d1ee3d
sanamcha <mhr.sanam@gmail.com>
2022-03-30 21:49:02 -0700

    update api

--- frontend/react-jobly/src/api.js
+++ frontend/react-jobly/src/api.js
@@ -2,23 +2,12 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-/** API Class.
- *
- * Static class tying together methods used to get/send to to the API.
- * There shouldn't be any frontend-specific stuff here, and there shouldn't
- * be any API-aware stuff elsewhere in the frontend.
- *
- */
-
+//create jobly-api, get token
 class JoblyApi {
-  // the token for interactive with the API will be stored here.
   static token;
 
   static async request(endpoint, data = {}, method = "get") {
     console.debug("API Call:", endpoint, data, method);
-
-    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
-    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
     const params = (method === "get")
@@ -43,7 +32,6 @@ class JoblyApi {
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
 //added here for get-companies by name
   static async getCompanies(name) {
     let res = await this.request("companies", { name });
