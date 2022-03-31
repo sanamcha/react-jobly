7670e0e6c2bde2e71179d64650b9d5f7b2d87313
sanamcha <mhr.sanam@gmail.com>
2022-03-30 21:51:48 -0700

    initial commit for jobcard

---
+++ frontend/react-jobly/src/jobs/JobCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
+
+//JobCardList -> JobCard
+
+function JobCard ({ id, title, companyName, Salary, equity }) {
+    console.log("JobCard");
+
+    return (
+        <div>
+            <h1>This is Jobcard page....</h1>
+        </div>
+    )
+
+
+}
+
+export default JobCard;
