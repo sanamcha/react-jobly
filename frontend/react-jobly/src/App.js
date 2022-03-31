53d7b512aede2a5c910e4df3ef2c3c97ddc0dbf5
sanamcha <mhr.sanam@gmail.com>
2022-03-31 08:24:36 -0700

    final commit for app

--- frontend/react-jobly/src/App.js
+++ frontend/react-jobly/src/App.js
@@ -1,48 +1,96 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { NavLink, Switch, Route, Redirect } from "react-router-dom";
+import Routes from "./routes/Routes";
+import Navigation from "./routes/Navigation";
 import JoblyApi from "./api";
 import './App.css';
-// import UserContext from "./form/userContext";
-import CompanyList from "./companies/CompanyList";
-import JobList from "./jobs/JobList";
-import CompanyDetail from "./companies/CompanyDetail";
+import UserContext from "./users/UserContext";
+import useLocalStorage from "./hooks/useLocalStorage";
+import jwt from "jsonwebtoken";
 
 //storing token in local-storage
 export const TOKEN_ID = "jobly-token";
 
 function App() {
-  // const [currentUser, setCurrentUser] = useState([]);
+  const [infoLoaded, setInfoLoaded] = useState(false);
+  const [currentUser, setCurrentUser] = useState([]);
+  const [applicationIds, setApplicationIds] = useState(new Set([]));
+  const [token, setToken] = useLocalStorage(TOKEN_ID);
 
+  useEffect(function loadUser(){
+    async function getCurrentUser(){
+      if(token) {
+        try{
+          let {username} = jwt.decode(token);
+          JoblyApi.token = token;
+          let currentUser = await JoblyApi.getCurrentUser(username);
+          setCurrentUser(currentUser);
+          setApplicationIds(new Set(currentUser.applications));
+        } catch (err) {
+          setCurrentUser(null);
+        }  
+      }
+      setInfoLoaded(true);
+    }
+    setInfoLoaded(false);
+    getCurrentUser();
+  }, [token]);
 
+//handle for logout
+function logout(){
+  setCurrentUser(null);
+  setToken(null);
+}
 
+//handle signup
+async function signup(signupData) {
+  try {
+    let token = await JoblyApi.signup(signupData);
+    setToken(token);
+    return { success: true };
+  } catch (errors) {
+    console.error("signup failed", errors);
+    return { success: false, errors };
+  }
+}
+
+//handle for login
+async function login(loginData) {
+  try {
+    let token = await JoblyApi.login(loginData);
+    setToken(token);
+    return { success: true };
+  } catch (errors) {
+    console.error("login failed", errors);
+    return { success: false, errors };
+  }
+}
+//to check jobs applied
+function hasApplied(id) {
+  return applicationIds.has(id);
+}
+
+//apply to job
+function apply(id) {
+  if(hasApplied(id)) return;
+  JoblyApi.apply(currentUser.username, id);
+  setApplicationIds(new Set([...applicationIds, id]));
+}
 
   return (
    <BrowserRouter>
-   <Switch >
-    {/* <UserContext.Provider */}
-      {/*  value={{ currentUser}} */}
-    {/*  > */}
+    <UserContext.Provider
+        value={{ currentUser, 
+        setCurrentUser,
+        hasApplied,
+        apply}} >
+  
       <div className="App">
-        {/* <Navigation />
-        <Routes /> */}
-      <NavLink to="/companies">Companies</NavLink>
-      <NavLink to="/jobs">Jobs</NavLink>
-      <Route exact path="/companies">
-        <CompanyList />
-      </Route>
-      <Route exact path="/jobs">
-        <JobList />
-      </Route>
-      <Route exact path="/companies/:handle">
-        <CompanyDetail />
-      </Route>
-      <Redirect to="/" />
-        <h1>HOMEPAGE.......</h1>
+        <Navigation logout={logout} />
+        <Routes login={login} signup={signup} />
       </div>
-    {/* </UserContext.Provider> */}
-    </Switch>
+    </UserContext.Provider>
    </BrowserRouter>
   );
 }
