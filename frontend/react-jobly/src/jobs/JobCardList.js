0c841022ba5b4805d1d169d51b664ca9b8979a2d
sanamcha <mhr.sanam@gmail.com>
2022-03-30 21:52:39 -0700

    initial commit for job card list

---
+++ frontend/react-jobly/src/jobs/JobCardList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function JobCardList(){
+
+
+    return (
+        <div>
+            <h1>
+                This is JobCardList page......
+            </h1>
+        </div>
+    )
+}
+
+export default JobCardList;
