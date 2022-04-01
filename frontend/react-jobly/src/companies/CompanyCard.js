fcde970b5d29c7bd2415bf6498035c4eefc75f16
sanamcha <mhr.sanam@gmail.com>
2022-04-01 12:05:08 -0700

    updated commit

--- frontend/react-jobly/src/companies/CompanyCard.js
+++ frontend/react-jobly/src/companies/CompanyCard.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./CompanyCard.css";
+
 
 
 
 function CompanyCard({ name, description, logoUrl, handle }) {
-    console.log("CompanyCard", logoUrl);
+    console.debug("CompanyCard", logoUrl);
 return (
-    <Link className="CompanyCard" to={`/companies/${handle}`}>
-        <div>
-            <h6>
+    <Link className="CompanyCard card" to={`/companies/${handle}`}>
+        <div className="card-body">
+            <h6 className="card-title">
                 {name}
-                {logoUrl && <img src={logoUrl}
-                alt={name} />}
+                {logoUrl && 
+                <img src={logoUrl}
+                     alt={name} className="float-right ml-4" />}
             </h6>
             <p>{description}</p>
         </div>
